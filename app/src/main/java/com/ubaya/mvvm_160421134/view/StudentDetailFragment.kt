62cb9ffeb885e0f4dc9b9fa803c40644a9760a9c
WIRA <s160421134@student.ubaya.ac.id>
2024-03-21 15:00:34 +0700

    Homework Week 4, DetailViewModel add dan code, dimunculkan di fragment_student_detail.xml

--- app/src/main/java/com/ubaya/mvvm_160421134/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/mvvm_160421134/view/StudentDetailFragment.kt
@@ -5,20 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ubaya.mvvm_160421134.databinding.FragmentStudentDetailBinding
+import com.ubaya.mvvm_160421134.viewmodel.DetailViewModel
+
 
 class StudentDetailFragment : Fragment() {
     private lateinit var binding: FragmentStudentDetailBinding
+    private lateinit var viewModel: DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentStudentDetailBinding.inflate(inflater,container,false)
+        binding = FragmentStudentDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer { student ->
+            binding.txtId.setText(student.id)
+            binding.txtName.setText(student.name)
+            binding.txtBod.setText(student.bod)
+            binding.txtPhone.setText(student.phone)
+            binding.txtUrl.setText(student.photoUrl)
+        })
+
+
+        binding.btnUpdate.setOnClickListener {
+
+        }
     }
 }
