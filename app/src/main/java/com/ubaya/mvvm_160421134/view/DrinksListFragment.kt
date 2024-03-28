7cfb5832712a497e6cf0678e4cbcfa0d058d9a2d
WIRA <s160421134@student.ubaya.ac.id>
2024-03-28 14:12:38 +0700

    Exercise W6, model data drink add, viewmodel edit, DrinkListFragment add

---
+++ app/src/main/java/com/ubaya/mvvm_160421134/view/DrinksListFragment.kt
@@ -0,0 +1,19 @@
+package com.ubaya.mvvm_160421134.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ubaya.mvvm_160421134.R
+class DrinksListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_drinks_list, container, false)
+    }
+
+}
--- app/src/main/java/com/ubaya/mvvm_160421134/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/ubaya/mvvm_160421134/viewmodel/ListViewModel.kt
@@ -11,10 +11,13 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.ubaya.mvvm_160421134.model.Drinks
 import com.ubaya.mvvm_160421134.model.Student
 
 class ListViewModel(application: Application): AndroidViewModel(application) {
     val studentsLD = MutableLiveData<ArrayList<Student>>()
+    val DrinksLD = MutableLiveData<ArrayList<Drinks>>()
+    val DrinksLoadErrorLD = MutableLiveData<Boolean>()
     val studentLoadErrorLD = MutableLiveData<Boolean>()
     val loadingLD = MutableLiveData<Boolean>()
 
@@ -22,25 +25,25 @@ class ListViewModel(application: Application): AndroidViewModel(application) {
     private var queue: RequestQueue? = null
 
     fun refresh() {
-        studentLoadErrorLD.value = false
+        DrinksLoadErrorLD.value = false
         loadingLD.value = true
 
         queue = Volley.newRequestQueue(getApplication())
-        val url = "http://adv.jitusolution.com/student.php"
+        val url = "http://10.0.2.2/drink/drink.json"
 
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             {
                 val sType = object : TypeToken<List<Student>>() { }.type
                 val result = Gson().fromJson<List<Student>>(it, sType)
-                studentsLD.value = result as ArrayList<Student>?
+                DrinksLD.value = result as ArrayList<Drinks>?
                 loadingLD.value = false
 
                 Log.d("show_volley", it)
             },
             {
                 Log.e("show_volley", it.toString())
-                studentLoadErrorLD.value = false
+                DrinksLoadErrorLD.value = false
                 loadingLD.value = false
             })
         stringRequest.tag = TAG
