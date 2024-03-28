075fba772f1372a0d8e85f9b7112383d69f9ff6d
WIRA <s160421134@student.ubaya.ac.id>
2024-03-28 15:10:24 +0700

    Exercise W6, layout drink_list_item.xml, code DrinkListAdapter dan DrinksListFragment.kt

--- app/src/main/java/com/ubaya/mvvm_160421134/view/DrinksListFragment.kt
+++ app/src/main/java/com/ubaya/mvvm_160421134/view/DrinksListFragment.kt
@@ -5,15 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ubaya.mvvm_160421134.R
+import com.ubaya.mvvm_160421134.databinding.FragmentDrinksListBinding
+import com.ubaya.mvvm_160421134.viewmodel.ListViewModel
+
 class DrinksListFragment : Fragment() {
+    private lateinit var viewModel:ListViewModel
+    private val drinkListAdapter  = DrinkListAdapter(arrayListOf())
+    private lateinit var binding: FragmentDrinksListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drinks_list, container, false)
+        binding = FragmentDrinksListBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recView.layoutManager = LinearLayoutManager(context)
+        binding.recView.adapter = drinkListAdapter
+
+        observeViewModel()
+
+        binding.refreshLayout.setOnRefreshListener {
+            binding.recView.visibility = View.GONE
+            binding.txtError.visibility = View.GONE
+            binding.progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            binding.refreshLayout.isRefreshing = false
+        }
+
+    }
+    fun observeViewModel() {
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
+
+    }
 }
